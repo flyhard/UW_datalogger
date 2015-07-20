7479e3858d39b2df4c225dbb16a4da5da655d755
Per Abich <per.abich@gmail.com>
2015-07-20 17:38:15 +0200

    Cleaning up and added button and state LED for turning on/off the serial output.

--- RTC_SDCARD_TEMP/RTC_SDCARD_TEMP.ino
+++ RTC_SDCARD_TEMP/RTC_SDCARD_TEMP.ino
@@ -1,5 +1,3 @@
-#include <DallasTemperature.h>
-
 // Date, Time and Alarm functions using a DS3231 RTC connected via I2C and Wire lib
 
 #include <Wire.h>
@@ -8,24 +6,88 @@
 #include <avr/sleep.h>
 #include <RTClib.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 
 
-RTC_DS3231 RTC;
 const int INTERRUPT_PIN = 2; //interupt is on pin D2
 const int INTERRUPT_NO = 0; // interupt is internally called 0
 const int ONEWIRE_PIN = 8;
 const int chipSelect = 10;
-volatile int state = LOW;
+const int SERIAL_TOGGLE = 3;
+const int SERIAL_TOGGLE_INT = 1;
+const int SERIAL_STATE_LED = 4;
+volatile boolean serialOn = true;
+volatile unsigned long button_time = 0;
+volatile unsigned long last_button_time = 0;
+volatile boolean serialTogglePressed = false;
+volatile boolean wakeupPressed = false;
+RTC_DS3231 RTC;
 DeviceAddress TempSensorAddr = { 0x28, 0x8B, 0x42, 0x5C, 0x06, 0x00, 0x00, 0x7D};
-OneWire  ds(ONEWIRE_PIN);  // Connect your 1-wire device(tempSensor) to pin 8
+OneWire ds(ONEWIRE_PIN);  // Connect your 1-wire device(tempSensor) to pin 8
 DallasTemperature sensors(&ds);
 
+void initSerial() {
+  pinMode(SERIAL_TOGGLE, INPUT);
+  pinMode(SERIAL_STATE_LED, OUTPUT);
+  digitalWrite(SERIAL_STATE_LED, HIGH);
+  digitalWrite(SERIAL_TOGGLE, HIGH);
+  attachInterrupt(SERIAL_TOGGLE_INT, toggleSerial, LOW);
+  Serial.begin(9600);
+  serialOn = true;
+}
+
+void toggleSerial() {
+  serialTogglePressed = true;
+}
+
+void setSerialMode() {
+  if (digitalRead(SERIAL_TOGGLE) == LOW) {
+    serialOn = !serialOn;
+    if (serialOn) {
+      Serial.begin(9600);
+      Serial.println("Serial on");
+    } else {
+      Serial.println("Serial off");
+      Serial.end();
+    }
+    blinkLED(3);
+    delay(1000);
+  }
+}
+
+void blinkLED(const int count) {
+  for (int i = 0; i < count; i++) {
+    if (serialOn) {
+      digitalWrite(SERIAL_STATE_LED, LOW);
+      delay(200);
+      digitalWrite(SERIAL_STATE_LED, HIGH);
+      delay(200);
+    } else {
+      digitalWrite(SERIAL_STATE_LED, HIGH);
+      delay(200);
+      digitalWrite(SERIAL_STATE_LED, LOW);
+      delay(200);
+    }
+  }
+  delay(1000);
+}
+
+void print(const char text[]) {
+  if (serialOn)
+    Serial.print(text);
+}
+
+void println(const char text[]) {
+  if (serialOn)
+    Serial.println(text);
+}
+
 void setup () {
+  initSerial();
   pinMode(INTERRUPT_PIN, INPUT);
   //pull up the interrupt pin
   digitalWrite(INTERRUPT_PIN, HIGH);
 
-  Serial.begin(9600);
 
   discoverOneWireDevices();
 
@@ -36,32 +98,32 @@ void setup () {
   //RTC.adjust(DateTime(__DATE__, __TIME__));
   setNewAlarm(2);
 
-  Serial.print("Initializing SD card...");
+  print("Initializing SD card...");
 
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+    println("Card failed, or not present");
     // don't do anything more:
     return;
   }
-  Serial.println("card initialized.");
-  Serial.println("setup done");
+  println("card initialized.");
+  println("setup done");
   sensors.begin();
-  Serial.print("Parasite power is: ");
+  print("Parasite power is: ");
   if (sensors.isParasitePowerMode())
-    Serial.println("ON");
+    println("ON");
   else
-    Serial.println("OFF");
+    println("OFF");
   // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
   sensors.setResolution(TempSensorAddr, 9);
-  Serial.print("Device 0 Resolution: ");
-  Serial.print(sensors.getResolution(TempSensorAddr), DEC);
-  Serial.println();
-  Serial.print("Waiting for conversions: ");
+  print("Device 0 Resolution: ");
+  if (serialOn)Serial.println(sensors.getResolution(TempSensorAddr), DEC);
+  if (serialOn)Serial.println();
+  print("Waiting for conversions: ");
   if (sensors.getWaitForConversion())
-    Serial.println("TRUE");
+    println("TRUE");
   else
-    Serial.println("FALSE");
+    println("FALSE");
 
 }
 
@@ -71,45 +133,47 @@ void discoverOneWireDevices(void) {
   byte data[12];
   byte addr[8];
 
-  Serial.print("Looking for 1-Wire devices...\n\r");
+  println("Looking for 1-Wire devices...\n\r");
   while (ds.search(addr)) {
-    Serial.print("\n\rFound \'1-Wire\' device with address:\n\r");
+    print("\n\rFound \'1-Wire\' device with address:\n\r");
     for ( i = 0; i < 8; i++) {
-      Serial.print("0x");
+      print("0x");
       if (addr[i] < 16) {
-        Serial.print('0');
+        if (serialOn)Serial.print('0');
       }
-      Serial.print(addr[i], HEX);
+      if (serialOn)Serial.print(addr[i], HEX);
       if (i < 7) {
-        Serial.print(", ");
+        print(", ");
       }
     }
     if ( OneWire::crc8( addr, 7) != addr[7]) {
-      Serial.print("CRC is not valid!\n");
+      println("CRC is not valid!\n");
       return;
     }
   }
-  Serial.print("\n\r\n\rThat's it.\r\n");
+  println("\n\r\n\rThat's it.\r\n");
   ds.reset_search();
   return;
 }
 
-void writeToSD(String dataString) {
+void writeToSD(const String dataString) {
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
 
   if (dataFile) {
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
+    blinkLED(1);
   }
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening datalog.txt");
+    println("error opening datalog.txt");
+    blinkLED(5);
   }
-    Serial.println(dataString);
+  if (serialOn)Serial.println(dataString);
 }
 
-void setNewAlarm(int minutes) {
+void setNewAlarm(const int minutes) {
   DateTime now = RTC.now();
   int hour = now.hour();
   int minute = now.minute() + minutes;
@@ -123,41 +187,48 @@ void setNewAlarm(int minutes) {
   if (RTC.checkAlarmEnabled(1) ) {
     char text[25];
     sprintf(text, "Alarms Enabled for %02d:%02d", hour, minute);
-    Serial.println(text);
+    println(text);
   }
   delay(3000);
 }
 
 void loop () {
-  char text[100];
   DateTime now = RTC.now();
   if (RTC.checkIfAlarm(1)) {
-    Serial.println("Alarm Triggered");
+    println("Alarm Triggered");
   }
 
-  Serial.println("Going to Sleep");
+  println("Going to Sleep");
   delay(600);
   sleepNow();
-  Serial.println("AWAKE");
-  setNewAlarm(1);
+  println("AWAKE");
+  if (serialTogglePressed) {
+    setSerialMode();
+    serialTogglePressed = false;
+  }
+  if (wakeupPressed) {
+    setNewAlarm(1);
 
-  //do something quick, flip a flag, and handle in loop();
-  Serial.println("======================================");
-  Serial.println("Log data");
-  now = RTC.now();
-  ds.reset();
-  if (sensors.isConnected(TempSensorAddr))
-    Serial.println("Sensor ready");
-  else
-    Serial.println("Sensor NOT ready");
-  sensors.requestTemperatures();
-  float temp = sensors.getTempC(TempSensorAddr);
-  char str_temp[6];
-  /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
-  dtostrf(temp, 4, 2, str_temp);
-  sprintf(text, "%4d-%02d-%02dT%02d:%02d:%02d, %sC", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second(), str_temp);
-  writeToSD(text);
-  Serial.println("======================================");
+    //do something quick, flip a flag, and handle in loop();
+    println("======================================");
+    println("Log data");
+    now = RTC.now();
+    ds.reset();
+    if (sensors.isConnected(TempSensorAddr))
+      println("Sensor ready");
+    else
+      println("Sensor NOT ready");
+    sensors.requestTemperatures();
+    float temp = sensors.getTempC(TempSensorAddr);
+    char str_temp[6];
+    /* 4 is mininum width, 2 is precision; float value is copied onto str_temp*/
+    dtostrf(temp, 4, 2, str_temp);
+    char text[30];
+    sprintf(text, "%4d-%02d-%02dT%02d:%02d:%02d, %sC", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second(), str_temp);
+    writeToSD(text);
+    println("======================================");
+    wakeupPressed = false;
+  }
 }
 
 void sleepNow() {
@@ -172,4 +243,5 @@ void sleepNow() {
 
 void logData() {
   // Intentionally left blank
+  wakeupPressed = true;
 }
